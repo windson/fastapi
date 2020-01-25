87f30460d50787b5766828da2aca42ae54f23f8b
navule <pavanraonavule@gmail.com>
2020-01-25 19:15:15 +0530

    'init'

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
+
+@app.get("/")
+def home():
+    return {"message":"Hello TutLinks.com"}
