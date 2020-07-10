fdbb0fa93c4a4d02addc008297fd5d549f3ed68a
navule <PavanKumar.Navule@cdk.com>
2020-07-10 17:21:08 +0530

    Dev Setup

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# fastapi
-FastAPI Tutorials
+# Deploy FastAPI on Azure
+
+FastAPI with Async REST API with PostgreSQL on Azure App Service 
+
+## Setup this Repo on Local PC
+
+In `main.py` comment the following line
+
+```python
+DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username,db_password, host_server, db_server_port, database_name, ssl_mode)
+```
+
+and uncomment the following line
+
+```python
+DATABASE_URL = "sqlite:///./test.db"
+```
+
+Replace following code
+
+```python
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, pool_size=20, max_overflow=0
+)
+```
+
+with 
+
+```python
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+```
+
+
+### Windows Users
+In command terminal run the following command
+```shell
+python -m venv env
+env/Scripts/activate
+python -m pip install -U pip
+pip install -r requirements.txt
+```
+
+### Linux Users
+In command terminal run the following command
+```shell
+python3 -m venv env
+source ./env/bin/activate
+python -m pip install -U pip
+pip install -r requirements.txt
+```
+
+## Run this app on Local PC
+In command terminal run the following command
+```shell
+uvicorn main:app --reload
+```
+
+
+## Deployment command
+
+```shell
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+```
---
+++ main.py
@@ -0,0 +1,89 @@
+from typing import List
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+import urllib
+
+DATABASE_URL = "sqlite:///./test.db"
+
+host_server = os.environ.get('host_server', 'localhost')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
+database_name = os.environ.get('database_name', 'fastapi')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+#DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
+app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
+
+@app.put("/notes/{note_id}/", response_model=Note)
+async def update_note(note_id: int, payload: NoteIn):
+    query = notes.update().where(notes.c.id == note_id).values(text=payload.text, completed=payload.completed)
+    await database.execute(query)
+    return {**payload.dict(), "id": note_id}
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+@app.get("/notes/{note_id}/", response_model=List[Note])
+async def read_notes(note_id: int):
+    query = notes.select().where(notes.c.id == note_id)
+    return await database.fetch_all(query)
+
+@app.delete("/notes/{note_id}/")
+async def update_note(note_id: int):
+    query = notes.delete().where(notes.c.id == note_id)
+    await database.execute(query)
+    return {"message": "Note with id: {} deleted successfully!".format(note_id)}
