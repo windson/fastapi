438bf2597331695239459b16396a25a025df5154
TonyLiao <h770320@gmail.com>
2022-01-22 18:55:23 +0800

    env

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+   
+
+    return {  os.environ["LineChannelAccessToken"] }
